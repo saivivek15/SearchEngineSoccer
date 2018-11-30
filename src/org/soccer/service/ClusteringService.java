603e96dfce63b8602c692fa1fdf866032d5eb626
Satya <sobellan@tetrationanalytics.com>
2018-11-30 00:33:40 -0600

    included single link clustering

--- src/org/soccer/service/ClusteringService.java
+++ src/org/soccer/service/ClusteringService.java
@@ -14,19 +14,19 @@ public class ClusteringService {
 		return FlatClustering.getFlatCluster(docResult);
 	}
 	
-	public ArrayList<DocEntity> getSingleClusterResult(ArrayList<DocEntity> docResult) throws Exception {
-		return HeirarClustering.getSingleLinkageCluster(docResult);
+	public ArrayList<DocEntity> getSingleClusterResult(ArrayList<DocEntity> docResult1) throws Exception {
+		return HeirarClustering.getSingleLinkageCluster(docResult1);
 	}
 	
-	public ArrayList<DocEntity> getCompleteClusterResult(ArrayList<DocEntity> docResult) throws Exception {
-		return HeirarClustering.getCompleteLinkageCluster(docResult);
+	public ArrayList<DocEntity> getCompleteClusterResult(ArrayList<DocEntity> docResult2) throws Exception {
+		return HeirarClustering.getCompleteLinkageCluster(docResult2);
 	}
 	
-	public ArrayList<DocEntity> getWeightedClusterResult(ArrayList<DocEntity> docResult) throws Exception {
-		return HeirarClustering.getWeightedLinkageCluster(docResult);
+	public ArrayList<DocEntity> getWeightedClusterResult(ArrayList<DocEntity> docResult3) throws Exception {
+		return HeirarClustering.getWeightedLinkageCluster(docResult3);
 	}
 	
-	public ArrayList<DocEntity> getAvgClusterResult(ArrayList<DocEntity> docResult) throws Exception {
-		return HeirarClustering.getAverageLinkageCluster(docResult);
+	public ArrayList<DocEntity> getAvgClusterResult(ArrayList<DocEntity> docResult4) throws Exception {
+		return HeirarClustering.getAverageLinkageCluster(docResult4);
 	}
 }
