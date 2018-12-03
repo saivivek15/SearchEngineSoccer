9444c388b163d18e0e96c7877cd22b22867a9845
Satya <sobellan@tetrationanalytics.com>
2018-12-02 19:09:42 -0600

    raw data clustering modified

--- src/org/soccer/clustering/KMeansClustering.java
+++ src/org/soccer/clustering/KMeansClustering.java
@@ -1,53 +1,108 @@
 package org.soccer.clustering;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Set;
 import java.util.TreeMap;
 import java.util.TreeSet;
 
 public class KMeansClustering {
-	
+
 	public ArrayList<String> fileNames = new ArrayList<>();
 	public ArrayList<String> totalTokens = new ArrayList<>();
 	public HashSet<String> tmpTotalTokens = new HashSet<>();
 	public ArrayList<ArrayList<String>> totalDocTokensList = new ArrayList<>();
 	public HashMap<String, Integer> outputMap = new HashMap<String, Integer>();
 	public ArrayList<ArrayList<Double>> docVectors = new ArrayList<>();
-	
-	public HashMap<String, Integer> performKMeansClustering(HashMap<String, String> inputMap) {
-		
-		fileNames.addAll(inputMap.keySet());
-		System.out.println("No of files: "+fileNames.size());
-		for(String fileName:fileNames) {
-		String[] tokens = inputMap.get(fileName).split(" ");
-		ArrayList<String> finalTokens = new ArrayList<String>(Arrays.asList(tokens));
-		tmpTotalTokens.addAll(finalTokens);
-		totalDocTokensList.add(finalTokens);
+	public Set<String> stopwordsSet = new HashSet<String>();
+
+	public static void main(String[] args) throws IOException{
+		KMeansClustering kmc = new KMeansClustering();
+		kmc.performKMeansClustering();
+	}
+
+	public HashMap<String, Integer> performKMeansClustering() throws IOException {
+
+		BufferedReader br0 = new BufferedReader(new FileReader("/Users/sobellan/Desktop/searchEngine/stopwords.txt"));
+		String stopWordLine = null;
+		while ((stopWordLine = br0.readLine()) != null) {
+			String[] words = stopWordLine.split(",\\s*");
+			this.stopwordsSet.addAll(Arrays.asList(words));
+		}
+
+		BufferedReader in;
+
+		File contentFolder = new File("/Users/sobellan/Downloads/data1");
+
+		File[] docFiles = contentFolder.listFiles();
+		Arrays.sort(docFiles);
+		for (File file : docFiles) {
+			if (file.isHidden()){
+				continue;
+			}
+			System.out.println(file.getName());
+			fileNames.add(file.getName());
+			in = new BufferedReader(new FileReader(file));
+			String line = null;
+			String content = "";
+
+			while ((line = in.readLine()) != null) {
+				content += line;
+			}
+			String processedFile = processText(content);
+			String delimiter = " ";
+			String[] tokens = processedFile.split(delimiter);
+			ArrayList<String> finalTokens = removeStopWords(tokens);
+			tmpTotalTokens.addAll(finalTokens);
+			totalDocTokensList.add(finalTokens);
+
 		}
 		totalTokens.addAll(tmpTotalTokens);
 		Collections.sort(totalTokens);
 
+		System.out.println("Filenames size: "+fileNames.size());
 		System.out.println("Total Unique words size: "+totalTokens.size());
-		
+		System.out.println(" Clustering started");
+
 		computeTfIdf();
 		TreeMap<Double, HashMap<ArrayList<Double>, TreeSet<Integer>>> errorSumMap = startKMeans();
-		
+
 		int clusterNo = 1;
 		for (ArrayList<Double> cent : errorSumMap.get(errorSumMap.lastKey()).keySet()) {
 			for (int pts : errorSumMap.get(errorSumMap.lastKey()).get(cent)) {
 				if (pts < fileNames.size()) {
 					outputMap.put(fileNames.get(pts), clusterNo);
 				}
-
 			}
 			clusterNo++;
 		}
 		System.out.println("Output Map: "+outputMap);
 		return outputMap;
 	}
+
+	public String processText(String str) {
+		str = str.replaceAll("\\<.*?>", " "); // to replace SGML tags with space
+		str = str.replaceAll("\\'s", ""); // remove posessives
+		str = str.replaceAll("[&+:;,=?@#|<>.'^$*()%\\!/\"]", ""); //remove special characters
+		str = str.replaceAll("-", " "); // replace - with space
+		str = str.replaceAll("\\s+", " "); // replace multiple spaces with single space
+		str = str.trim().toLowerCase();
+		return str;
+	}
+
+	public ArrayList<String> removeStopWords(String[] tokens) {
+		ArrayList<String> tokensList = new ArrayList<String>(Arrays.asList(tokens));
+		tokensList.removeAll(this.stopwordsSet);
+		return tokensList;
+	}
+
 	public void computeTfIdf() {		
 		for (ArrayList<String> tokenList : totalDocTokensList) {
 			ArrayList<Double> docVector = new ArrayList<Double>();
@@ -77,7 +132,6 @@ public class KMeansClustering {
 			int iteations = 0;
 			while (true) {
 				clusters = new HashMap<ArrayList<Double>, TreeSet<Integer>>(tmpClusters);
-
 				// assign clusters to the documents
 				for (int i = 0; i < docVectors.size(); i++) {
 					ArrayList<Double> centroid = null;
@@ -95,7 +149,7 @@ public class KMeansClustering {
 
 				// update clusters centroid
 				tmpClusters.clear();
-				
+
 				for (ArrayList<Double> centroid : clusters.keySet()) {
 					double[] newCentroid = new double[centroid.size()];
 					ArrayList<Double> tmp = new ArrayList<Double>();
@@ -106,7 +160,7 @@ public class KMeansClustering {
 					}
 					for (int i = 0; i < newCentroid.length; i++)
 						newCentroid[i] /= clusters.get(centroid).size();
-					
+
 					for (double d:newCentroid)
 						tmp.add(d);
 					tmpClusters.put(tmp, new TreeSet<Integer>());
@@ -114,7 +168,7 @@ public class KMeansClustering {
 
 				// check end conditions and stop clustering
 				iteations++;
-				
+
 				ArrayList<Double> oldCentroidsList = new ArrayList<Double>();
 				ArrayList<Double> newCentroidsList = new ArrayList<Double>();
 				for (ArrayList<Double> x : clusters.keySet())
@@ -130,13 +184,13 @@ public class KMeansClustering {
 					break;
 			} // end of while
 
-//			System.out.println(clusters.values());
-//			System.out.println(clusters.toString().replaceAll("\\[[\\w@]+=", ""));
-//			if (iteations < maxIterations)
-//				System.out.println("Converged in " + iteations + " steps.");
-//			else
-//				System.out.println("Stopped after " + maxIterations + " iterations.");
-//			System.out.println("");
+			//			System.out.println(clusters.values());
+			//			System.out.println(clusters.toString().replaceAll("\\[[\\w@]+=", ""));
+			//			if (iteations < maxIterations)
+			//				System.out.println("Converged in " + iteations + " steps.");
+			//			else
+			//				System.out.println("Stopped after " + maxIterations + " iterations.");
+			//			System.out.println("");
 
 			// calculate similarity sum and map it to the clustering
 			double similaritySum = 0;
@@ -153,12 +207,12 @@ public class KMeansClustering {
 			tmpClusters.clear();
 			kcenters.clear();
 		} // end of main for loop
-//		HashMap<ArrayList<Double>, TreeSet<Integer>> d = errorSumMap.lastEntry().getValue();
-//		for (ArrayList<Double> dd:d.keySet()){
-//			System.out.println("__________________");
-//			for (double ddd:d.get(dd))
-//				System.out.println(ddd+1);
-//		}
+		//		HashMap<ArrayList<Double>, TreeSet<Integer>> d = errorSumMap.lastEntry().getValue();
+		//		for (ArrayList<Double> dd:d.keySet()){
+		//			System.out.println("__________________");
+		//			for (double ddd:d.get(dd))
+		//				System.out.println(ddd+1);
+		//		}
 		return errorSumMap;
 	}
 
